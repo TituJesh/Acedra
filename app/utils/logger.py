2d99ce6d715f881fa68448a8843fec418503cfea
TituJesh <titujesh@gmail.com>
2026-09-18 16:13:14 +0530

    Add application logger utility

---
+++ app/utils/logger.py
@@ -0,0 +1,15 @@
+import logging
+
+logger = logging.getLogger("acedra")
+
+logger.setLevel(logging.INFO)
+
+handler = logging.StreamHandler()
+
+formatter = logging.Formatter(
+    "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+)
+
+handler.setFormatter(formatter)
+
+logger.addHandler(handler)
