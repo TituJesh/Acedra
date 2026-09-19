ef02b4e167558d7486a418a00f40b59146c82b17
TituJesh <titujesh@gmail.com>
2026-09-19 21:20:38 +0530

    Add application startup log in main app

--- app/main.py
+++ app/main.py
@@ -3,8 +3,10 @@ from app.routers.auth import router as auth_router
 from app.routers.students import router as students_router
 from app.routers.departments import router as departments_router
 from app.routers import documents
+from app.utils.logger import logger
 
 app = FastAPI(title="Acedra")
+logger.info("Acedra application started")
 app.include_router(auth_router)
 app.include_router(students_router)
 app.include_router(departments_router)
